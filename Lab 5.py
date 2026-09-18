d8863e68661fe57802e4e40211fe0f41eb93901a
Danie1Alexandre <dani.alexl@live.se>
2026-09-18 16:10:09 +0200

    remade L5- A3

--- Lab 5.py
+++ Lab 5.py
@@ -1,51 +1,69 @@
-def print_separator():
-     print("_"*40)
 
 # Part A - Scope
 
-
 #1
 
-course_name = "python"
+# course_name = "python"
 
-def courses():
-    course_name = "Ai"
-    print(course_name)
-
-
-courses() #prints a local variabel
-#specifik for the function couses
-
-print(course_name) #prints a Global variabel
-
-print_separator()
+# def courses():
+#     course_name = "Ai"
+#     print(course_name)
 
 
-#2
-count = 0 #Global
+# courses() #prints a local variabel
+# #specifik for the function couses
 
-def counter():
-    count = 0 # Local
-    count += 1
-    print (count)
+# print(course_name) #prints a Global variabel
 
-#same variabel name but count stay on zero on golbal
+
+
+
+# #2
+# count = 0 #Global
+
+# def counter():
+#     count = 0 # Local
+#     count += 1
+#     print (count)
+
+# #same variabel name but count stay on zero on golbal
     
-counter()
-print(count)
+# counter()
+# print(count)
 
 #3
 
-greet = "hello"
-name = "ada"
+score = 10  #global
+
+#there will be a error
+#the function dont have a value for score
+def add_score():  
+    score +=1
+    print("score")
+#add_score()
+
+#solution
+def new_add_score(score):  
+    return score + 1
+
+print (new_add_score(score))
 
 
-def name_format(Name):
-    greet = "Good Day"
-    print(name, greet)
 
-name_format(name)
-print(greet)
+
+#4
+
+
+
+
+# Part G- Stretch challenges
+#1 
+
+
+
+
+
+
 
 
 
