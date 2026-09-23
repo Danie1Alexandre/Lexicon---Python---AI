50a28bca6bfc8339af56d8d4264ee0fefbdbdd2d
Danie1Alexandre <dani.alex@live.se>
2026-09-23 14:04:18 +0200

    L8 - A4

--- Lab 8.py
+++ Lab 8.py
@@ -1,20 +1,44 @@
 # Part A - Mutable default arguments
 
-#1 
+#1 --------
 
 class BadTeam:
     def __init__(self, name, members = []): #bad
         self.name = name
-        self.members = members
+        self.members = members 
 
     def add_member(self,member):
         self.members.append(member)
 
-
+#2------
 club1 = BadTeam("aik")
 club2 = BadTeam("dif")
 
 club1.add_member("anna")
 
-print(club1.members) # anna add on both
+print(club1.members) 
 print(club2.members)
+# anna added on both clubs, since both clubs uses the same list
+
+# 3---------------
+
+class GoodTeam:
+    def __init__(self, name, members = None): #bad
+        self.name = name
+        if members == None:
+            members = []
+            
+        self.members = members
+
+    def add_member(self,member):
+        self.members.append(member)
+
+#4--------------
+club1 = GoodTeam("aik")
+club2 = GoodTeam("dif")
+
+club1.add_member("anna")
+
+print("good", club1.members) 
+print(club2.members)
+
